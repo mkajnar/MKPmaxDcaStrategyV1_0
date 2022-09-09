04372bd1b1afd4e24742f971d0276908fde7cf13
mkajnar <fotografie@mmphoto.cz>
2022-09-09 16:03:36 +0200

    First Commit

--- hyperopt2.sh
+++
@@ -1,3 +0,0 @@
-docker exec -it $(docker ps -qf "name=CntPMAX_TEST") freqtrade hyperopt --print-all --print-json -e 5000 -c /freqtrade/user_data/configs/configPMAX.json -d user_data/data/kucoin --hyperopt-loss SharpeHyperOptLossDaily --strategy $1 --spaces {buy,roi,trailing} --timerange $2
-
-
